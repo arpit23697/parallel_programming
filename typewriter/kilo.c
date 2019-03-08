e0ae28a9fc50a8231e17e05efa1a2ac713007b9d
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-03-08 18:14:46 +0530

    preventing from falling of the screen : upto 49

--- typewriter/kilo.c
+++ typewriter/kilo.c
@@ -183,15 +183,19 @@ void editorMoveCursor(int key)
 {
     switch(key){
         case ARROW_LEFT:
+            if (E.cx != 0)
             E.cx--;
             break;
         case ARROW_RIGHT:
+            if (E.cx != E.screencols - 1)
             E.cx++;
             break;
         case ARROW_UP:
+            if (E.cy != 0)
             E.cy--;
             break;
         case ARROW_DOWN:
+            if (E.cy != E.screenrows - 1)
             E.cy++;
             break;
     }
