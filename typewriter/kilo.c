a85a5fc17fec305be96ffd4d308200368e899041
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-03-08 18:46:19 +0530

    Home , end , delete : upto 54

--- typewriter/kilo.c
+++ typewriter/kilo.c
@@ -17,6 +17,9 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 
@@ -124,8 +127,13 @@ int editorReadKey (){
                 if (read(STDERR_FILENO, &seq[2] , 1) != 1) return '\x1b';
                 if (seq[2] == '~'){
                     switch(seq[1]){
+                        case '1' : return HOME_KEY;
+                        case '3' : return DEL_KEY;
+                        case '4' : return END_KEY;
                         case '5' : return PAGE_UP;
                         case '6' : return PAGE_DOWN;
+                        case '7' : return HOME_KEY;
+                        case '8' : return END_KEY;
                     }
                 }
             }
@@ -135,10 +143,19 @@ int editorReadKey (){
                 case 'B': return ARROW_DOWN;
                 case 'C': return ARROW_RIGHT;
                 case 'D': return ARROW_LEFT;
-            }
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+                }
             }
         }
-
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
+            }
+        }
         return '\x1b';
     }
     else
@@ -226,6 +243,13 @@ void editorProcessKeypress () {
             exit(0);
             break;
 
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
         case PAGE_UP:
         case PAGE_DOWN:
         {
