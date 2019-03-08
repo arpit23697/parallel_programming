1ec5b303950955ca1000dcaa286f9eb8f1f181a2
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-03-08 17:13:30 +0530

    printed the welcome message : upto 42

--- typewriter/kilo.c
+++ typewriter/kilo.c
@@ -9,6 +9,9 @@
 
 // *********************************** defines ********************
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+#define Arpit_editor_version "0.0.1"
+
 // ********************************** append buffer ***************************
 
 //this struct is for creating the dynamic string with only one operation append
@@ -165,8 +168,25 @@ void editorDrawRows( struct abuf *ab){
     //printing the tildas upto last line - 1 and then printing simply the tilda at the last
     for (int y = 0 ; y < E.screenrows ; y++)
     {
-        abAppend(ab , "~" , 1);
+        if (y == E.screenrows / 3){
+            char welcome[80];
+            int welcomelen = snprintf(welcome , sizeof(welcome) , 
+            "Arpit Editor -- version %s" , Arpit_editor_version);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
 
+            // for centering the welcome message
+            int padding = (E.screencols - welcomelen)/2;
+            if (padding){
+                abAppend(ab , "~" , 1);
+                padding--;
+            }
+            while(padding--)
+                abAppend(ab , " " , 1);
+            abAppend(ab, welcome , welcomelen);
+        }
+        else{
+            abAppend(ab , "~" , 1);
+        }
         abAppend(ab , "\x1b[K" , 3);                    //print the tilda and then clear the line to the right of the cursor
                                                        //by defualt it ersases to the right of the cursor
         if (y < E.screenrows - 1){
