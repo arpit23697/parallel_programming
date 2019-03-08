320f855f15a5cb48729a3a1312fcddc76d3949eb
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-03-08 12:25:58 +0530

    printing the tildas ; upto 35

--- typewriter/kilo.c
+++ typewriter/kilo.c
@@ -30,8 +30,8 @@ struct editorConfig E;
 // *********************************** terminal *******************************
 void die (char *s)
 {
-    write(STDOUT_FILENO , "\x1b[2J" , 4);
-    write(STDERR_FILENO, "\x1b[H" , 3);
+    write(STDOUT_FILENO , "\x1b[2J" , 4);                 //clears the screen
+    write(STDERR_FILENO, "\x1b[H" , 3);                   //place the cursor at the top left
     perror(s);
     exit(1);
 }
@@ -60,37 +60,74 @@ void enableRawMode () {
     if (tcsetattr(STDIN_FILENO , TCSAFLUSH , &raw) == -1) die("tcsetattr");    
 }
 
+// reads a single key press and then return the result
 char editorReadKey (){
     int nread;
     char c;
-    while ((nread = read(STDERR_FILENO , &c , 1)) != 1){
-        if (nread == -1 && errno != EAGAIN) die("read");
+
+    while ((nread = read(STDERR_FILENO , &c , 1)) != 1){   //this will read the character; put number of characters read in nread; while loop will continue until 1 character is read
+        if (nread == -1 && errno != EAGAIN) die("read");   //give the error message if failed in reading
     }
     
     return c;
 }
 
+//get the position of the cursor
+int getCursorPosition (int *rows , int *cols)
+{
+    char buf[32];
+    unsigned int i =0;
+
+
+    if (write(STDOUT_FILENO , "\x1b[6n" , 4) != 4) return -1;           //this queries the cursor position
+                                                                //first we place the cursor at the bottom right 
+                                                                // and then ask the cursor position to get the size of the terminal
+    printf("\r\n");
+    char c;
+    //reads the query response until R is encountered and put it in the buffer buf
+    while (i < sizeof(buf) - 1){
+        if (read(STDIN_FILENO , &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+
+    // for printing the buffer
+    buf[i] = '\0';
+    printf("\r\n&buf[1] : '%s'\r\n" , &buf[1]);           //it starts with buf[1] because 0th character is the escape sequence
+    
+    //parsing the buffer for the cursor position; returning the error if not able to read properly
+    //second line ; parse a string of the form (int;int)
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2] , "%d;%d" , rows,cols) != 2) return -1;
+
+    return 0;
+}
+
+// gets the size of the window and put it in rows and cols
 int getWindowSize (int *rows , int *cols)   {
     struct winsize ws;
-    if (ioctl(STDOUT_FILENO , TIOCGWINSZ , &ws) == -1 || ws.ws_col == 0){
-        return -1;
+    if (ioctl(STDOUT_FILENO , TIOCGWINSZ , &ws ) == -1 || ws.ws_col == 0){
+        if (write(STDOUT_FILENO , "\x1b[999C\x1b[999B" , 12) != 12) return -1;        //C is for moving right and B is for moving downword
+        return getCursorPosition(rows, cols);
     }
+
     else
     {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0;
-    }
-    
+    }    
 }
 
 // *****************************input *****************************
+//this function map the control sequences to the appropriate function
 void editorProcessKeypress () {
     char c = editorReadKey();
+    // printf("%c" , c);
     switch (c){
         case CTRL_KEY('q'):
-            write(STDERR_FILENO , "\x1b[2J" , 4);
-            write(STDERR_FILENO , "\x1b[H" , 3);
+            write(STDERR_FILENO , "\x1b[2J" , 4);              //for clearing the screen
+            write(STDERR_FILENO , "\x1b[H" , 3);               //for placing the cursor at the top of the screen
             exit(0);
             break;
     }
@@ -98,8 +135,10 @@ void editorProcessKeypress () {
 
 // ****************************** output ********************************
 void editorDrawRows(){
-    for (int y = 0 ; y < E.screenrows ; y++)
+    for (int y = 0 ; y < E.screenrows -1 ; y++)
         write(STDOUT_FILENO , "~\r\n" , 3);
+    
+    write(STDOUT_FILENO , "~" , 1);
 }
 
 
@@ -125,8 +164,8 @@ int main()
   char c;
   
   while (1){
-      editorRefreshScreen();
-      editorProcessKeypress();
+      editorRefreshScreen();                //refreshes the screen
+      editorProcessKeypress();              //take the key press and process it 
   }
     return 0;
 
