87fab12b036beca341792c11cbc014e3c4b39df6
arpit23697 <111601031@smail.iitpkd.ac.in>
2019-03-08 18:25:58 +0530

    commit for page up and page down : upto 51

--- typewriter/kilo.c
+++ typewriter/kilo.c
@@ -16,7 +16,9 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 
 };
 // ********************************** append buffer ***************************
@@ -114,14 +116,26 @@ int editorReadKey (){
 
         if (read(STDIN_FILENO , &seq[0] , 1) != 1) return '\x1b';
         if (read(STDIN_FILENO , &seq[1] , 1) != 1) return '\x1b';
-
+        
         if (seq[0] == '[')
         {
+            if (seq[1] >= '0' && seq[1] <= '9')
+            {
+                if (read(STDERR_FILENO, &seq[2] , 1) != 1) return '\x1b';
+                if (seq[2] == '~'){
+                    switch(seq[1]){
+                        case '5' : return PAGE_UP;
+                        case '6' : return PAGE_DOWN;
+                    }
+                }
+            }
+            else{
             switch(seq[1]){
                 case 'A': return ARROW_UP;
                 case 'B': return ARROW_DOWN;
                 case 'C': return ARROW_RIGHT;
                 case 'D': return ARROW_LEFT;
+            }
             }
         }
 
@@ -211,6 +225,17 @@ void editorProcessKeypress () {
             write(STDERR_FILENO , "\x1b[H" , 3);               //for placing the cursor at the top of the screen
             exit(0);
             break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            int times = E.screenrows;
+            while (times--){
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+        }
+        break;
+
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
